79768396b888649fbd1ac31846f370c61e682f1b
dylankb <dylan.barnard@gmail.com>
2017-09-23 23:34:08 -0700

    Style header view

--- public/javascripts/templatesCompiled.js
+++ public/javascripts/templatesCompiled.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['mainHeader'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"left-group\">\n  <div class=\"back-button\">Back\n  </div>\n  <div class=\"search\">Search\n  </div>\n</div>\n\n<a href=\"/\">\n  <span class=\"logo\">Trello</span>\n</a>\n\n<div class=\"right-group\">\n  <div class=\"plus-button\">+\n  </div>\n  <div class=\"notifications\">\n  </div>\n  <div class=\"user-badge\">D\n  </div>\n</div>\n";
+    return "<div class=\"left-group\">\n  <div class=\"header-button header-md back-button\">\n    <i class=\"fa fa-arrow-left\" aria-hidden=\"true\"></i>\n  </div>\n  <div class=\"header-button search\">\n    <input class=\"search-input\" type=\"text\">\n  </div>\n</div>\n\n<a class=\"logo\" href=\"/\">\n  <span class=\"logo-icon\"></span>\n</a>\n\n<div class=\"right-group\">\n  <div class=\"header-button header-md plus-button\">\n    <i class=\"fa fa-plus\" aria-hidden=\"true\"></i>\n  </div>\n  <div class=\"header-button header-md notifications\">\n    <i class=\"fa fa-bell\" aria-hidden=\"true\"></i>\n  </div>\n  <div class=\"header-button header-md user-badge\">D\n  </div>\n</div>\n";
 },"useData":true});
 })();
